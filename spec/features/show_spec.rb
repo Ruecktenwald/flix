940f67c1532709145ad92be5ff05ba006d4e515b
Peter Ruecktenwald <ruecktenwald@gmail.com>
2019-02-21 22:21:15 -0500

    finish tests - no 10.

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -4,12 +4,7 @@ describe do "going to a movie page"
 
 	it "shows the movie details" do
 
-		movie = Movie.create(
-				title: "Catwoman",
-				rating: "R",
-				description: "She wears a lot of leather as she steals diamonds.",
-				released_on: "2019-01-20",
-				total_gross: "300550000.00")
+		movie = Movie.create(movie_attributes(total_gross: 300000000.00))
 
 	visit movie_url(movie)
 
@@ -18,6 +13,28 @@ describe do "going to a movie page"
 		expect(page).to have_text(movie.rating)
 		expect(page).to have_text(movie.description)
 		expect(page).to have_text(movie.released_on)
-		expect(page).to have_text("$300,550,000.00")
 	end
+
+
+	it "shows the total gross if the total gross exceeds $50M" do
+		movie = Movie.create(total_gross: 51000000.00)
+
+		visit movie_url(movie)
+
+		expect(page).to have_text("$51,000,000.00")
+	end
+
+
+	it "shows 'Flop!' if the total gross is less than $50M" do
+
+	movie = Movie.create(total_gross: 49000000.00)
+
+		visit movie_url(movie)
+
+		expect(page).to have_text("Flop!")
+
+
+	end
+
+
 end
