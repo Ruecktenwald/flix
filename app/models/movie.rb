f8dd8067bc642d542a73ad36e1b51cf511c5070f
Peter Ruecktenwald <ruecktenwald@gmail.com>
2019-02-28 18:44:00 -0500

    add migration -reviews-, create review feature

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@
 
 
 class Movie < ApplicationRecord
-  
+
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
@@ -14,7 +14,7 @@ class Movie < ApplicationRecord
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: { in: RATINGS }
 
-
+  has_many :reviews , dependent: :destroy
 
 
 
